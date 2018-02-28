5ab2e2d63d42b2ce1e741bc96d3d0021afb9e924
Juan Daniel  Martinez Ramirez <juan.martinez@eisei.net.mx>
2018-02-27 18:13:57 -0600

    Correcion

--- app/proyectos.php
+++ app/proyectos.php
@@ -11,12 +11,11 @@ class proyectos extends Model
     protected $table="proyectos";
     protected $primaryKey = 'idProyecto';
 
-   protected $fillable = ['nombre', 'descCorta', 'descLarga','imagenUrl','videoUrl','metaMin','metaMax','fechaInicio','fechaFin', 'idProducto','idUsuario','idMercado','numeroClientes','inversion','valorMercado', 'descComollegarClientes', 'propuestaValor', 'idMiembro'];
-
-   public function setImagenUrlAttribute($imagenUrl) {
-   		$this->attributes['imagenUrl'] = Carbon::now()->second.$imagenUrl->getClientOriginalName();
-   		$name = Carbon::now()->second.$imagenUrl->getClientOriginalName();
-   		\Storage::disk('local')->put($name, \File::get($imagenUrl));
-   }
+    protected $fillable = ['nombre', 'descCorta', 'descLarga','imagenUrl','videoUrl','metaMin','metaMax','fechaInicio','fechaFin', 'idProducto','idUsuario','idMercado','numeroClientes','inversion','valorMercado', 'descComollegarClientes', 'propuestaValor', 'idMiembro'];  
+ 
+   public function setImagenUrlAttribute($imagenUrl) { 
+       $this->attributes['imagenUrl'] = Carbon::now()->second.$imagenUrl->getClientOriginalName(); 
+       $name = Carbon::now()->second.$imagenUrl->getClientOriginalName(); 
+       \Storage::disk('local')->put($name, \File::get($imagenUrl)); 
+   } 
 }
-
